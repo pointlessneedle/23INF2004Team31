17574b28ac5f1e3189c8e452f720782310767589
yichingt <62843208+yichingt@users.noreply.github.com>
2023-11-08 19:03:25 +0800

    Update project.c

--- flashID/project.c
+++ flashID/project.c
@@ -18,11 +18,31 @@
 #define PAGE_SIZE       256     // Page Size reading
 #define SECTOR_SIZE     4096
 
-#define MAX_SIZE 15
+#define MAX_SIZE 38
 
-int size = 0;   // current number of elements in map
-char keys[MAX_SIZE][20];    // array to store keys
-char values[MAX_SIZE][50];      // array to store values
+// Array to store keys / hexadecimal instruction returned by flash
+char keys[MAX_SIZE][20] = {"0x3010","0x3011","0x3012","0x3013",
+                    "0x4012","0x4013","0x4014","0x4015",
+                    "0x4016","0x4017","0x4018","0x4019", 
+                    "0x4020","0x4021","0x6011","0x6012",
+                    "0x6013","0x6014","0x6015","0x6016",
+                    "0x6017","0x6018","0x6019","0x6020",
+                    "0x6021","0x7015","0x7016","0x7017",
+                    "0x7018","0x7019","0x7022","0x8015",
+                    "0x8016","0x8017","0x8018","0x8019",
+                    "0x8020", "-1"};
+
+// Array to store values / flash type
+char values[MAX_SIZE][150] = {"W25X05CL","W25X10CL","W25X20CL","W25X40CL",
+                      "W25Q20CL","W25Q40CL","W2525Q80/DV","W25Q16/JL or W25Q16JV-IQ",
+                      "W25Q32 or W25Q32JV-IQ/JQ","W25Q64FV (SPI) or W25Q64JV-IQ/JQ","W25Q128BV or W25Q128JV-IQ/JQ","W25Q256FV (SPI Mode) or W25Q256JV-IQ",
+                      "W25Q512JV-IQ/IN","W25Q01JV-IQ","W25Q10EW","W25Q20EW",
+                      "W25Q40EW","W25Q80EW","W25Q16JW-IQ/JQ","W25Q32FW or W25Q32JW-IQ",
+                      "W25Q64FV (QPI) or W25Q64JW-IQ", "W25Q128FV (QPI Mode) or W25Q128FW or W25Q128JW-IQ", "W25Q256FV (QPI Mode) or W25Q257FV (QPI Mode) or W25Q256JW", "W25Q512NW-IQ/IN", 
+                      "W25Q01NW-IQ/JQ", "W25Q16JV-IM","W25Q32Jv-IM*/JM*","W25Q64JV-IM/JM",
+                      "25Q128JV-IM/JM","W25Q256JV-IM","W25Q02JV-IM","W25Q16JW-IM",
+                      "W25Q32JW-IM","W25Q64JW-IM","W25Q128JW-IM","W25Q256JW-IM",
+                      "W25Q512NW-IM", "-1"};
 
 static inline void cs_select(uint cs_pin){
     gpio_put(cs_pin, 0);
@@ -33,25 +53,12 @@ static inline void cs_deselect(uint cs_pin){
 
 // Get the index of a key in the keys array
 int getIndex(char key[]){
-    for (int i = 0; i < size; i++) { 
+    for (int i = 0; i < MAX_SIZE; i++) { 
         if (strcmp(keys[i], key) == 0) { 
             return i; 
         } 
     } 
     return -1; // Key not found 
-}
-
-// to insert a key-value pair into the map, when wanting to add new flash series to the list
-void add_item(char key[], char value[]){
-    int index = getIndex(key); 
-    if (index == -1) { // Key not found 
-        strcpy(keys[size], key); 
-        strcpy(values[size], value);
-        size++; 
-    } 
-    else { // Key found 
-        strcpy(values[index], value); 
-    }    
 }
 
 // To get value of a key in the map
@@ -65,32 +72,12 @@ char* check_item(char key[])
     else {
         // Key found 
         return values[index]; 
-    } 
-} 
-
-void insert_all_to_list(){
-    add_item("-1","-1");    // Key not found
-    add_item("0x4014","W25Q80");
-    add_item("0x4015","W25Q16");
-    add_item("0x4016","W25Q32 or W25Q32JV-IQ/JQ");
-    add_item("0x4017","W25Q64FV (SPI)");
-    add_item("0x4018","W25Q128BV");
-    add_item("0x4019","W25Q256FV (SPI Mode) or W25Q256FV (SPI Mode)");
-    add_item("0x4021","W25Q01JV-IQ");
-
-    add_item("0x6016","W25Q32FW");
-    add_item("0x6017","W25Q64FV (QPI)");
-    add_item("0x6018","W25Q128FV (QPI Mode) or W25Q128FW");
-    add_item("0x6019","W25Q256FV (QPI Mode) or W25Q257FV (QPI Mode)");
-    add_item("0x6021","W25Q01NW-IQ/JQ");
-    
-    add_item("0x7016","W25Q32Jv-IM*/JM*");
-    add_item("0x7022","W25Q02JV-IM");
+    }
 }
 
 void printMap() 
 { 
-    for (int i = 0; i < size; i++) { 
+    for (int i = 0; i < MAX_SIZE; i++) { 
         printf("%s: %s\n", keys[i], values[i]); 
     } 
 } 
@@ -120,36 +107,27 @@ void __not_in_flash_func(read_chip_id)(spi_inst_t *spi, uint cs_pin) {
     uint8_t buffer3 = rx_buffer[3]; // 2 and 3 = deviceID
     uint8_t buffer4 = rx_buffer[4]; // chip id address
 
-    /* Checks */
-    printf("check: 0x%02X\n", buffer1);
-    printf("check2: 0x%02X\n", buffer2);
-    printf("check3: 0x%02X\n", buffer3);
-    printf("check4: 0x%02X\n", buffer4);
-
     // Combining buffer2 and buffer3 together to get deviceID
     uint16_t device_id = ((uint16_t)buffer2 << 8) | buffer3;
-    printf("Device ID: 0x%02X\n", device_id);
+    //printf("Device ID: 0x%02X\n", device_id);
 
     if ( buffer4 & CHIP_ID ){
-        printf("Flash Type: Winbond Serial Flash\n");
+        printf("\n== Details of connected flash ==\nFlash Type: Winbond Serial Flash\n");
     }
 
-    /* HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE */
+    // Check if the device ID is under the list of known flash type as defined above
     char device_id_str[7];
     snprintf(device_id_str, sizeof(device_id_str), "0x%02X", device_id);
-
     if (check_item(device_id_str) != NULL){
-        printf("Device ID: 0x%02X\nWinbond Part #: %s\n", device_id, check_item(device_id_str));
+        printf("Manufacturer ID: 0x%02X\nDevice ID: 0x%02X\nWinbond Part #: %s\n", (uint16_t)buffer1, device_id, check_item(device_id_str));
     } else {
         printf("Unknown flash type.");
     }
-
-    sleep_ms(20);
-    printf("Ready\n");
 }
 
 int main() {
-    stdio_init_all();
+    //stdio_init_all();
+    stdio_usb_init();
     // set the baud rate to 1MHz
     spi_init(SPI_PORT, BAUD_RATE);
     spi_set_baudrate(SPI_PORT, BAUD_RATE);
@@ -164,17 +142,13 @@ int main() {
     gpio_set_dir(SPI_CS_PIN, GPIO_OUT);
     
     sleep_ms(1000);
-    // Inserting the list
-    insert_all_to_list();
 
     sleep_ms(5000);
     printf("Starting...\n");
-
-    printMap();
+    //printMap();
 
     sleep_ms(2000);
     read_chip_id(SPI_PORT, SPI_CS_PIN);
 
-
-
+    return 0;
 }
