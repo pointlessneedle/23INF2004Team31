d6cf4b4ece963fbee80d60eafb29d60a8a38ffe4
yichingt <62843208+yichingt@users.noreply.github.com>
2023-11-22 22:57:34 +0800

    some extra stuff
    added some further steps that can be taken, improvement from the partial demo as commented by profs

--- flashmicro/flashmicro.c
+++ flashmicro/flashmicro.c
@@ -16,15 +16,19 @@
 
 #define READ_DATA       0x03    // Address Read Data
 #define CHIP_ID         0x9F    // Address Status Read ID
+#define CHIP_ID_2       0X9E    // Second Address Status Read ID for Micron Chips
+#define READ_STATUS_REG 0x05    // Address Read Status
+#define BUSY_STATUS     0x01    // Address Busy Status
 
 #define BAUD_RATE       1000*1000
 #define PAGE_SIZE       256     // Page Size reading
 #define SECTOR_SIZE     4096
 
-#define MAX_SIZE 38
+#define W_MAX_SIZE 38   // For Winbond
+#define M_MAX_SIZE 13   // For Micron
 
 // Array to store keys / hexadecimal instruction returned by flash
-char keys[MAX_SIZE][20] = {"0x3010","0x3011","0x3012","0x3013",
+char w_keys[W_MAX_SIZE][20] = {"0x3010","0x3011","0x3012","0x3013",
                     "0x4012","0x4013","0x4014","0x4015",
                     "0x4016","0x4017","0x4018","0x4019", 
                     "0x4020","0x4021","0x6011","0x6012",
@@ -36,7 +40,7 @@ char keys[MAX_SIZE][20] = {"0x3010","0x3011","0x3012","0x3013",
                     "0x8020", "-1"};
 
 // Array to store values / flash type
-char values[MAX_SIZE][150] = {"W25X05CL","W25X10CL","W25X20CL","W25X40CL",
+char w_values[W_MAX_SIZE][150] = {"W25X05CL","W25X10CL","W25X20CL","W25X40CL",
                       "W25Q20CL","W25Q40CL","W2525Q80/DV","W25Q16/JL or W25Q16JV-IQ",
                       "W25Q32 or W25Q32JV-IQ/JQ","W25Q64FV (SPI) or W25Q64JV-IQ/JQ","W25Q128BV or W25Q128JV-IQ/JQ","W25Q256FV (SPI Mode) or W25Q256JV-IQ",
                       "W25Q512JV-IQ/IN","W25Q01JV-IQ","W25Q10EW","W25Q20EW",
@@ -47,8 +51,19 @@ char values[MAX_SIZE][150] = {"W25X05CL","W25X10CL","W25X20CL","W25X40CL",
                       "W25Q32JW-IM","W25Q64JW-IM","W25Q128JW-IM","W25Q256JW-IM",
                       "W25Q512NW-IM", "-1"};
 
+char m_keys[M_MAX_SIZE][20] = {"0xBA20","0xBB18","0xBB19","0xBA22",
+                        "0xBA21","0xBA19","0xBA19", "0xBA18",
+                        "0xBA17", "0xBB22", "0xBB21", "0xBB20",
+                        "0xBB17"};
+
+char m_values[M_MAX_SIZE][150] = {"N25Q512A or MT25QU02GCBB-3V-512Mb","N25Q128A or MT25QU02GCBB-1.8V-128Mb","N25Q256A or MT25QU02GCBB-1.8V-1Gb", "MT25QU02GCBB-3V-2Gb",
+                        "MT25QU02GCBB-3V-1Gb", "MT25QU02GCBB-3V-256Mb", "MT25QU02GCBB-3V-128Mb",
+                        "MT25QU02GCBB-3V-64Mb", "MT25QU02GCBB-1.8V-2Gb", "MT25QU02GCBB-1.8V-1Gb", "MT25QU02GCBB-1.8V-512Mb",
+                        "MT25QU02GCBB-1.8V-64Mb"};
+
 // Structure to hold the flash ID information
 typedef struct {
+    char type[40];
     uint8_t buffer1;
     uint16_t device_id;
     char device_id_part[150];
@@ -174,6 +189,7 @@ void write_to_new_file(char *file_name, char *data) {
 
 int day, month, year;
 void get_system_time(){
+    // To be changed manually
     uint64_t current_time = time_us_64();
     struct tm* time_info = gmtime((time_t*)&current_time);
     sleep_ms(10);
@@ -181,7 +197,7 @@ void get_system_time(){
         // Extract the day, month and year within the system, and change to fit current date
         year = time_info->tm_year + 1953; // Years since 1900
         month = time_info->tm_mon + 8; // Month is 0-indexed
-        day = time_info->tm_mday + 8;
+        day = time_info->tm_mday + 11;
         // Output print DD:MM:YY
         printf("Current System Time: %02d:%02d:%02d\n", day, month, year);
     } else {
@@ -222,37 +238,61 @@ static inline void cs_deselect(uint cs_pin){
     gpio_put(cs_pin, 1);
 }
 
-// Function to get the index of a key in the keys array
-int getIndex(char key[]){
-    for (int i = 0; i < MAX_SIZE; i++) { 
-        if (strcmp(keys[i], key) == 0) { 
-            return i; 
+// Function to get the index of a key in the keys array. int 1 = Winbond, 2 = Micron
+int getIndex(char key[], int a){
+    if (a == 1){
+        for (int i = 0; i < W_MAX_SIZE; i++) { 
+            if (strcmp(w_keys[i], key) == 0) { 
+                return i; 
+            } 
         } 
-    } 
-    return -1; // Key not found 
-}
-
-// Function to get value of a key in the map
-char* check_item(char key[]) 
-{ 
-    int index = getIndex(key); 
-    if (index == -1) {
-        // Key not found
-        return NULL;
-    } 
-    else {
-        // Key found 
-        return values[index]; 
+        return -1; // Key not found
+    } else if (a == 2) {
+        for (int i = 0; i < W_MAX_SIZE; i++) { 
+            if (strcmp(m_keys[i], key) == 0) { 
+                return i; 
+            } 
+        } 
+        return -1; // Key not found        
     }
 }
+
+// Function to get value of a key in the map. int 1 = Winbond, 2 = Micron
+char* check_item(char key[], int i) 
+{
+    if (i == 1){
+        int index = getIndex(key, 1); 
+        if (index == -1) {
+            // Key not found
+            return NULL;
+        } 
+        else {
+            // Key found 
+            return w_values[index]; 
+        }        
+    } else if(i == 2){
+        int index = getIndex(key, 2); 
+        if (index == -1) {
+            // Key not found
+            return NULL;
+        } 
+        else {
+            // Key found 
+            return m_values[index]; 
+        }          
+    }
+
+}
+
 // Function to list all the items
 void printAllItems()
 { 
-    for (int i = 0; i < MAX_SIZE; i++) { 
-        printf("%s: %s\n", keys[i], values[i]); 
+    for (int i = 0; i < W_MAX_SIZE; i++) { 
+        printf("%s: %s\n", w_keys[i], w_values[i]); 
     } 
 } 
 
+// Function to print the things in the memory's buffer 
 void printbuf(uint8_t buf[PAGE_SIZE]) {
     for (int i = 0; i < PAGE_SIZE; ++i) {
         if (i % 16 == 15)
@@ -262,7 +302,43 @@ void printbuf(uint8_t buf[PAGE_SIZE]) {
     }
 }
 
-// Main function to read the ID of flash chip
+void __not_in_flash_func(wait_busy)(spi_inst_t *spi, uint cs_pin){
+    uint8_t flag;
+    do{
+        cs_select(cs_pin);
+        uint8_t buf[2] = {READ_STATUS_REG, 0};
+        spi_write_read_blocking(spi, buf, buf, 2);
+        sleep_ms(20);
+        cs_deselect(cs_pin);
+        flag = buf[1];
+    }
+    while(flag & BUSY_STATUS);  // Binary check if flag and busy status is same
+}
+
+void __not_in_flash_func(read_flash)(spi_inst_t *spi, uint cs_pin, uint32_t addr, uint8_t *buf, size_t len) {
+    printf("Command: Reading Flash...\n");
+    cs_select(cs_pin);
+    
+    // Data to be read divided into 3 bytes, high | middle | low byte of address respectively
+    //
+    uint8_t cmdbuf[4] = {READ_DATA, addr >> 16, addr >> 8, addr};
+    spi_write_blocking(spi, cmdbuf, 4);
+    spi_read_blocking(spi, 0, buf, len);
+    sleep_ms(20);
+    cs_deselect(cs_pin);
+
+    printf("Ready\n");
+}
+
+// This code below will check against Winbond and Micron flash chips.
+// Please note that due to the unavailability of specific Micron Flash equipment
+// required for testing, the functionality of this code that is related to checking 
+// against Micron flash has not been fully validated. Once the necessary equipment
+// becomes available, further testing will be conducted, and any issues or incomplete
+// functionality will be addressed in subsequent development phases.
+// 
+
+// Main function to read the ID of flash chip. Includes both Winbond and Micron
 FlashID __not_in_flash_func(read_chip_id)(spi_inst_t *spi, uint cs_pin) {
     FlashID flash_id;
     printf("Command: Reading ID\n");
@@ -275,6 +351,7 @@ FlashID __not_in_flash_func(read_chip_id)(spi_inst_t *spi, uint cs_pin) {
     spi_write_read_blocking(spi, tx_buffer, rx_buffer, sizeof(tx_buffer));
 
     // Getting manufacturerID and deviceID
+    uint8_t buffer0 = rx_buffer[0];
     uint8_t buffer1 = rx_buffer[1]; // manufacturerID
     uint8_t buffer2 = rx_buffer[2]; // 2 and 3 = deviceID
     uint8_t buffer3 = rx_buffer[3]; // 2 and 3 = deviceID
@@ -282,35 +359,63 @@ FlashID __not_in_flash_func(read_chip_id)(spi_inst_t *spi, uint cs_pin) {
 
     // Combining buffer2 and buffer3 together to get deviceID
     uint16_t device_id = ((uint16_t)buffer2 << 8) | buffer3;
-    //printf("Device ID: 0x%02X\n", device_id);
 
-    if ( buffer4 & CHIP_ID ){
+    // Check if buffer4 matches Winbond ManufacturerID
+    if (buffer1 & 0xEF){
         printf("\n== Details of connected flash ==\n");
         sleep_ms(200);  // Sleep added to slow down the processing speed of the pico for print statement
         printf("Flash Type: Winbond Serial Flash\n");
+
+        // Check if the device ID is under the list of known flash type as defined above
+        char device_id_part[7];
+        snprintf(device_id_part, sizeof(device_id_part), "0x%02X", device_id);
+        if (check_item(device_id_part, 1) != NULL){
+            printf("Manufacturer ID: 0x%02X\n", (uint16_t)buffer1);
+            sleep_ms(200);
+            printf("Device ID: 0x%02X\n", device_id);
+            sleep_ms(200);
+            printf("Winbond Part #: %s\r\n\n", check_item(device_id_part, 1));
+
+            // Values to be returned to structure
+            snprintf(flash_id.type, sizeof(flash_id.type), "Flash Type: Winbond NOR Serial Flash\n"); 
+            flash_id.buffer1 = (uint16_t)buffer1;
+            flash_id.device_id = device_id;
+            snprintf(flash_id.device_id_part, sizeof(flash_id.device_id_part), "%s\r\n\n", check_item(device_id_part, 1));
+        } else {
+            printf("Unknown flash type.\r\n");
+        }
     }
+    // Check if buffer1 matches Micron ManufactuerID
+    else if (buffer0 & 0x20){
+        // Combine buffer1 and buffer2 to get deviceID
+        uint16_t device_id = ((uint16_t)buffer1 << 8) | buffer2;
+        printf("\n== Details of connected flash ==\n");
+        sleep_ms(200);  // Sleep added to slow down the processing speed of the pico for print statement
+        printf("Flash Type: Micron Serial Flash\n");
 
-    // Check if the device ID is under the list of known flash type as defined above
-    char device_id_part[7];
-    snprintf(device_id_part, sizeof(device_id_part), "0x%02X", device_id);
-    if (check_item(device_id_part) != NULL){
-        printf("Manufacturer ID: 0x%02X\n", (uint16_t)buffer1);
-        sleep_ms(200);
-        printf("Device ID: 0x%02X\n", device_id);
-        sleep_ms(200);
-        printf("Winbond Part #: %s\r\n\n", check_item(device_id_part));
+        // Check if the device ID is under the list of known flash type as defined above
+        char device_id_part[7];
+        snprintf(device_id_part, sizeof(device_id_part), "0x%02X", device_id);
+        if (check_item(device_id_part, 2) != NULL){
+            printf("Manufacturer ID: 0x%02X\n", (uint16_t)buffer1);
+            sleep_ms(200);
+            printf("Device ID: 0x%02X\n", device_id);
+            sleep_ms(200);
+            printf("Micron Part #: %s\r\n\n", check_item(device_id_part, 2));
 
-        // Values to be returned to structure
-        char buffer1_str[10];
-        char device_id_str[10];
-        char device_id_part_str[150];
-        
-        flash_id.buffer1 = (uint16_t)buffer1;
-        flash_id.device_id = device_id;
-        snprintf(flash_id.device_id_part, sizeof(flash_id.device_id_part), "%s\r\n\n", check_item(device_id_part));
-    } else {
+            // Values to be returned to structure
+            snprintf(flash_id.type, sizeof(flash_id.type), "Flash Type: Micron NOR Serial Flash\n"); 
+            flash_id.buffer1 = (uint16_t)buffer1;
+            flash_id.device_id = device_id;
+            snprintf(flash_id.device_id_part, sizeof(flash_id.device_id_part), "%s\r\n\n", check_item(device_id_part, 2));
+        } else {
+            printf("Unknown flash type.\r\n");
+        }
+    }
+    else{
         printf("Unknown flash type.\r\n");
     }
+
     return flash_id;    // Return values into the structure
 }
 
@@ -345,12 +450,15 @@ int main() {
     initialise_sd();
     get_system_time();
     
+    /* Here maybe can put to a function instead */
     // Collating statements and content of flash id to save into file
     char to_sd_output[20];
+    char type_output[40];
     char buffer1_str_output[25];
     char device_id_str_output[20];
     char device_id_part_str_output[170];
     snprintf(to_sd_output, sizeof(to_sd_output), "%02d_%02d_%02d.txt", day, month, year);
+    snprintf(type_output, sizeof(type_output), flashid.type); 
     snprintf(buffer1_str_output, sizeof(buffer1_str_output), "Manufacturer ID: 0x%02X\n", (uint16_t)flashid.buffer1);
     snprintf(device_id_str_output, sizeof(device_id_str_output), "Device ID: 0x%02X\n", flashid.device_id);
     snprintf(device_id_part_str_output, sizeof(device_id_part_str_output), "Winbond Part #: %s\r\n\n", flashid.device_id_part);
@@ -358,12 +466,32 @@ int main() {
     sleep_ms(10);
     
     // Printing
-    printf("\nSaving content to microsd card...\n");
+    printf("\nCommand: Saving content to microsd card...\n");
     write_to_new_file(to_sd_output, buffer1_str_output);
+    write_to_existing_file(to_sd_output, type_output);    
     write_to_existing_file(to_sd_output, device_id_str_output);
     write_to_existing_file(to_sd_output, device_id_part_str_output);
     sleep_ms(10);
     printf("\n\n=== Content Saved ===\n");
+
+    sleep_ms(10);
+
+    /* Here maybe can save to a function instead */
+    // To get the content in the memory buffer, defined by page and target_address defined to start from 0
+    printf("\n=== Getting Memory Buffer Content ===\n\n");
+    uint8_t page_buf[PAGE_SIZE];
+    const uint32_t target_address = 0;
+    read_flash(SPI_PORT, SPI_CS_PIN, target_address, page_buf, PAGE_SIZE);
+    sleep_ms(10);
+    for(int i = 0; i < PAGE_SIZE; i++){
+        // For each bytes in size of page, write 100 (decimal integer) which is 64 in hexadecimal
+        // Can be changed to any integer you want
+        page_buf[i] = 100;
+    }
+    printbuf(page_buf);
+    //write_to_existing_file(to_sd_output, page_buf);
+    sleep_ms(10);
+    printf("\n=== Memory buffer content written to file ===\n\n");
 
     sleep_ms(10);
     // Unmount drive
